52cb9b2009bcbd5cf34bbc5b3e2f0aafb4bc2583
Amor <contact@amorkumar.com>
2021-11-19 00:20:08 -0500

    Change class to className

--- frontend/src/components/blocks/CallToAction/CallToAction.js
+++ frontend/src/components/blocks/CallToAction/CallToAction.js
@@ -12,27 +12,27 @@ export default function CallToAction({
   secondaryCta,
 }) {
   return (
-    <div class="max-w-6xl mx-auto mb-8 bg-gray-50">
-      <div class="max-w-5xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-        <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          <span class="block">{headingCall ?? ''}</span>
-          <span class="block text-indigo-600">{headingQuestion ?? ''}</span>
+    <div className="mb-8 bg-gray-50">
+      <div className="max-w-5xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <span className="block">{headingQuestion ?? ''}</span>
+          <span className="block text-indigo-600">{headingCall ?? ''}</span>
         </h2>
-        <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
-          <div class="inline-flex rounded-md shadow">
+        <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+          <div className="inline-flex rounded-md shadow">
             <a
               href={primaryCta?.url ?? ''}
               target={primaryCta?.target ?? ''}
-              class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
             >
               {primaryCta?.title ?? ''}
             </a>
           </div>
-          <div class="ml-3 inline-flex rounded-md shadow">
+          <div className="ml-3 inline-flex rounded-md shadow">
             <a
               href={secondaryCta?.url ?? ''}
               target={secondaryCta?.target ?? ''}
-              class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
+              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
             >
               {secondaryCta?.title ?? ''}
             </a>
